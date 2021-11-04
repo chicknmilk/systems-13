ddda58fab3809bd91841cdf6a7a1f281ab00cfbe
chicknmilk <aaronhsu4606@gmail.com>
2021-11-04 11:47:35 -0400

    headers

---
+++ boroughs.h
@@ -0,0 +1,15 @@
+#ifndef BOROUGHS_H
+#define BOROUGHS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+#endif
---
+++ main.c
@@ -0,0 +1,1 @@
+#include "boroughs.h"
