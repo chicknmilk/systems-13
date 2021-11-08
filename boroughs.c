0b3777e6a03a9a6b3eeff35b8d20990f3a8915d4
Aaron Hsu <aaronhsu4606@gmail.com>
2021-11-07 19:15:13 -0500

    Update

--- boroughs.c
+++ boroughs.c
@@ -107,7 +107,7 @@ void update_data() {
   printf("enter a data entry to update\n");
   fgets(data, sizeof(data), stdin);
 
-  struct pop_entry modified_entry = entries[atoi(data) - 1];
+  struct pop_entry * modified_entry = entries + atoi(data) - 1;
 
   struct pop_entry new_entry;
   int year, population;
@@ -122,8 +122,9 @@ void update_data() {
   sscanf(data2, "%d %s %d", &new_entry.year, new_entry.boro, &new_entry.population);
   printf("after scanf\n");
 
-  entries[atoi(data) - 1] = new_entry;
-  printf("after update\n");
+  modified_entry->year = new_entry.year;
+  strcpy(modified_entry->boro, new_entry.boro);
+  modified_entry->population = new_entry.population;
 
   int f_out = open("boroughs.data", O_WRONLY | O_CREAT, 0644);
   // write entries to file
